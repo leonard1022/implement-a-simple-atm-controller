22edd301e9a5b29c4ff99dc350d36131376516f8
leonard1022 <dlstlrl8704@gmail.com>
2025-09-17 16:48:07 +0900

    feat: TransactionRepository 인터페이스 추가

---
+++ src/main/kotlin/kr/co/wanted/implementasimpleatmcontroller/repository/TransactionRepository.kt
@@ -0,0 +1,11 @@
+package kr.co.wanted.implementasimpleatmcontroller.repository
+
+import kr.co.wanted.implementasimpleatmcontroller.domain.Account
+import kr.co.wanted.implementasimpleatmcontroller.domain.Transaction
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface TransactionRepository : JpaRepository<Transaction, Long> {
+    fun findByAccountOrderByTransactionDateDesc(account: Account): List<Transaction>
+}
