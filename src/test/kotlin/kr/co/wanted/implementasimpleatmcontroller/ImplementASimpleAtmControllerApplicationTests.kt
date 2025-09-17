eccd08bee86cb1c3f4f0d37003b90ef6486e21b0
leonard1022 <dlstlrl8704@gmail.com>
2025-09-17 15:51:38 +0900

    initial commit

---
+++ src/test/kotlin/kr/co/wanted/implementasimpleatmcontroller/ImplementASimpleAtmControllerApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.co.wanted.implementasimpleatmcontroller
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ImplementASimpleAtmControllerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
